065196f069efe6e640839600f51ae473a13ef410
michipce <michipce@gmail.com>
2024-04-10 07:59:37 +0200

    millora de paginacio

--- react/src/paginacion.jsx
+++ react/src/paginacion.jsx
@@ -1,19 +1,35 @@
-import  { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
 const PaginationExample = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [offset, setOffset] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
   const limit = 15; // Número de resultados por página
 
   useEffect(() => {
     fetchData();
-  }, [offset]);
+  }, [currentPage]);
+
+  useEffect(() => {
+    async function fetchPageCount() {
+      try {
+        const response = await axios.get('http://localhost:8000/api/registersTotals');
+        const totalUsers = response.data.count;
+        setPageCount(Math.ceil(totalUsers / limit));
+      } catch (error) {
+        console.error('Error fetching page count:', error);
+      }
+    }
+    fetchPageCount();
+  }, []);
 
   const fetchData = async () => {
     try {
       setLoading(true);
+      const offset = currentPage * limit;
       const response = await axios.get(`http://localhost:8000/api/users?limit=${limit}&offset=${offset}`);
       setUsers(response.data);
       setLoading(false);
@@ -22,16 +38,10 @@ const PaginationExample = () => {
       setLoading(false);
     }
   };
-console.log(offset)
-  const handlePrevPage = () => {
-    if (offset >= limit) { // Asegura que el offset no sea menor que 0
-      setOffset(offset - limit);
-    }
-  };
 
-
-  const handleNextPage = () => {
-    setOffset(offset + limit);
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected;
+    setCurrentPage(selectedPage); // Actualiza el estado de la página actual
   };
 
   return (
@@ -58,8 +68,20 @@ console.log(offset)
             ))}
           </tbody>
         </table>
-        <button onClick={handlePrevPage} disabled={offset === 0}>Anterior</button>
-        <button onClick={handleNextPage}>Siguiente</button>
+        {pageCount > 0 && ( // Condición para renderizar React-Paginate solo si pageCount es mayor que 0
+          <ReactPaginate
+            previousLabel={'Anterior'}
+            nextLabel={'Siguiente'}
+            breakLabel={'...'}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName={'pagination'} // Aplica la clase 'pagination'
+            activeClassName={'active'}
+            forcePage={currentPage} // Asegura que la página activa refleje el estado actual
+          />
+        )}
       </div>
       )}
     </div>
